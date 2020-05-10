855a528dad99c90dc8d4fc7e2a2d391922e61e2d
daidq <daidoquang@vccorp.vn>
2020-05-10 23:05:11 +0700

    tab-main-post style

--- vue_components/main-post.vue
+++ vue_components/main-post.vue
@@ -1,5 +1,37 @@
 <template>
-    <div>
-        
+    <div class="main-post">
+        <div class="tab-main-post">
+            <a href="javascript:;">
+                <i class="fa fa-picture-o" aria-hidden="true"></i>
+            </a>
+            <a href="javascript:;">
+                <i class="fa fa-play-circle-o" aria-hidden="true"></i>
+            </a>
+            <a href="javascript:;">
+                <i class="fa fa-smile-o" aria-hidden="true"></i>
+            </a>
+            <a href="javascript:;">
+                <i class="fa fa-bookmark-o" aria-hidden="true"></i>
+            </a>
+            <a href="javascript:;">
+                <i class="fa fa-link" aria-hidden="true"></i>
+            </a>
+            <a href="javascript:;">
+                <i class="fa fa-file-text-o" aria-hidden="true"></i>
+            </a>
+        </div>
+        <div class="box-post-type">
+            
+        </div>
     </div>
 </template>
+<script>
+    Vue.component('tab-main-post',  httpVueLoader('vue_components/tab-main-post.vue'));
+    module.exports = {
+        data: function() {
+            return{
+
+            }
+        },
+    };
+</script>
