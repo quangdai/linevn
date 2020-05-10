b58985cf72d88457e63a5a528182e84dc10d3ba7
daidq <daidoquang@vccorp.vn>
2020-05-10 16:40:48 +0700

    layout-style post-head ok

---
+++ vue_components/post-head.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="post-head">
+        <h5 class="title">Timeline post</h5>
+        <div class="btn-gr">
+            <button class="btn-draft" :disabled="isActive == 0">
+                Save draft
+            </button>
+            <button class="btn-schedule" :disabled="isActive == 0">
+                Schedule
+            </button>
+        </div>
+    </div>
+</template>
+<script>
+module.exports = {
+    data: function () {
+        return {
+            
+        }
+    },
+    methods: {
+
+    },
+    props: ['isActive']
+}
+</script>
