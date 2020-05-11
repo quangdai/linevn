283b508f4163e61783598327d3f6935fcfac69fd
daidq <daidoquang@vccorp.vn>
2020-05-11 23:24:38 +0700

    hover tab

---
+++ vue_components/image-post.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="image-post">
+
+    </div>
+</template>
--- vue_components/main-post.vue
+++ vue_components/main-post.vue
@@ -3,34 +3,40 @@
         <div class="tab-main-post">
             <a href="javascript:;">
                 <i class="fa fa-picture-o" aria-hidden="true"></i>
+                <span>Photo</span>
             </a>
             <a href="javascript:;">
                 <i class="fa fa-play-circle-o" aria-hidden="true"></i>
+                <span>Video</span>
             </a>
             <a href="javascript:;">
                 <i class="fa fa-smile-o" aria-hidden="true"></i>
+                <span>Sticker</span>
             </a>
             <a href="javascript:;">
                 <i class="fa fa-bookmark-o" aria-hidden="true"></i>
+                <span>Coupon</span>
             </a>
             <a href="javascript:;">
                 <i class="fa fa-link" aria-hidden="true"></i>
+                <span>Link</span>
             </a>
             <a href="javascript:;">
                 <i class="fa fa-file-text-o" aria-hidden="true"></i>
+                <span>Surveys</span>
             </a>
         </div>
         <div class="box-post-type">
-            
+            <image-post></image-post>
         </div>
     </div>
 </template>
 <script>
-    Vue.component('tab-main-post',  httpVueLoader('vue_components/tab-main-post.vue'));
+    Vue.component('image-post',  httpVueLoader('vue_components/image-post.vue'));
     module.exports = {
         data: function() {
             return{
-
+                
             }
         },
     };
